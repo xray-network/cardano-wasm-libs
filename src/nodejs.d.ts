9f4ae193d99e6bd68280d0e2bb3a7ff248082436
xray-robot <robot@xray.app>
2025-05-28 11:47:31 +0300

    Fix imports

--- src/nodejs.d.ts
+++ src/nodejs.d.ts
@@ -1,3 +1,3 @@
-export * as CML from "./libs/cardano-multiplatform-lib/nodejs/cardano_multiplatform_lib"
-export * as MSL from "./libs/message-signing-lib/nodejs/cardano_message_signing"
-export * as UPLC from "./libs/untyped-plutus-core/nodejs/uplc_wasm"
+export * as CML from "./libs/cardano-multiplatform-lib/nodejs"
+export * as MSL from "./libs/message-signing-lib/nodejs"
+export * as UPLC from "./libs/untyped-plutus-core/nodejs"
--- src/web.d.ts
+++ src/web.d.ts
@@ -1,3 +1,3 @@
-export * as CML from "./libs/cardano-multiplatform-lib/web/cardano_multiplatform_lib"
-export * as MSL from "./libs/message-signing-lib/web/cardano_message_signing"
-export * as UPLC from "./libs/untyped-plutus-core/web/uplc_wasm"
+export * as CML from "./libs/cardano-multiplatform-lib/web"
+export * as MSL from "./libs/message-signing-lib/web"
+export * as UPLC from "./libs/untyped-plutus-core/web"
