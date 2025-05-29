433143b336a06d7c8a823e2698cedf62632f815f
xray-robot <robot@xray.app>
2025-05-29 09:45:19 +0300

    Fixing imports

--- src/web.js
+++ src/web.js
@@ -1,14 +1,14 @@
-import * as CML from "./libs/cardano-multiplatform-lib/web"
+import * as CML from "./libs/cardano-multiplatform-lib/web/cardano_multiplatform_lib"
 import CMLWasm from "./libs/cardano-multiplatform-lib/web/cardano_multiplatform_lib_bg.wasm"
 CML.initSync(CMLWasm)
-export * as CML from "./libs/cardano-multiplatform-lib/web"
+export * as CML from "./libs/cardano-multiplatform-lib/web/cardano_multiplatform_lib"
 
-import * as MSL from "./libs/message-signing-lib/web"
+import * as MSL from "./libs/message-signing-lib/web/cardano_message_signing"
 import MSLWasm from "./libs/message-signing-lib/web/cardano_message_signing_bg.wasm"
 MSL.initSync(MSLWasm)
-export * as MSL from "./libs/message-signing-lib/web"
+export * as MSL from "./libs/message-signing-lib/web/cardano_message_signing"
 
-export * as UPLC from "./libs/untyped-plutus-core/web"
+import * as UPLC from "./libs/untyped-plutus-core/web/uplc_wasm"
 import UPLCWasm from "./libs/untyped-plutus-core/web/uplc_wasm_bg.wasm"
 UPLC.initSync(UPLCWasm)
-export * as UPLC from "./libs/untyped-plutus-core/web"
+export * as UPLC from "./libs/untyped-plutus-core/web/uplc_wasm"
