6a8b9269cd1cf0cee5e6e6536b60a3fbfe73ce73
xray-robot <robot@xray.app>
2025-05-29 09:08:18 +0300

    Update submodules script

--- scripts/msl-update.sh
+++ scripts/msl-update.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+SUBMODULE_DIR=rust/message-signing-lib
+cd $SUBMODULE_DIR
+git fetch
+git checkout
+git submodule update --init --recursive
+echo "✅ Updated submodule <<$SUBMODULE_DIR>> to latest commit"
